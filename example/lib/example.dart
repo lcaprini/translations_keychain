4f6bac1cf04bd4422673e636858225354d550a55
Luca Caprini <luca.caprini@eng.it>
2020-04-18 13:47:29 +0200

    Example updated

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -3,5 +3,18 @@ library example;
 import 'package:example/i18n/translations_keychain.dart';
 
 void main() {
+  // Simple label
   print(TranslationsKeychain.HELLO);
+
+  // Label with numbers
+  print(TranslationsKeychain.A_123);
+
+  // Label with spaces
+  print(TranslationsKeychain.LABEL_WITH_SPACES);
+
+  // Label with symbols
+  print(TranslationsKeychain.LABEL_WITH_SYMBOLS);
+
+  // Label with pluralization with [easy_localization] package
+  // print(TranslationsKeychain.CLICKED.plural(counter));
 }
--- example/lib/i18n/translations_keychain.dart
+++ example/lib/i18n/translations_keychain.dart
@@ -5,8 +5,19 @@
 // **************************************************************************
 
 abstract class TranslationsKeychain {
+  static const A_123 = '123';
+  static const CLICKED = 'CLICKED';
+  static const CLICKED_few = 'CLICKED.few';
+  static const CLICKED_many = 'CLICKED.many';
+  static const CLICKED_one = 'CLICKED.one';
+  static const CLICKED_other = 'CLICKED.other';
+  static const CLICKED_two = 'CLICKED.two';
+  static const CLICKED_zero = 'CLICKED.zero';
+  static const ERROR = 'ERROR';
   static const ERROR_WRONG_PASSWORD = 'ERROR.WRONG_PASSWORD';
   static const ERROR_WRONG_USERNAME = 'ERROR.WRONG_USERNAME';
   static const HELLO = 'HELLO';
+  static const LABEL_WITH_SPACES = 'LABEL WITH SPACES';
+  static const LABEL_WITH_SYMBOLS = 'LABEL.WITH:SYMBOLS';
   static const OTHER_LABEL = 'OTHER_LABEL';
 }
